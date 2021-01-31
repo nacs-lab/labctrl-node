fe779f382b569cd80e1f53fe79f8d384d754d75e
Yichao Yu <yyc1992@gmail.com>
2021-01-30 21:57:26 -0500

    [socket] Implement client API for call, get and set.

--- server/socket.js
+++ server/socket.js
@@ -20,8 +20,61 @@
 
 const Server = require('./server');
 
+const { is_object } = require('../lib/utils');
+
 class socket {
-    // TODO: implement a subset of API that can be used with direct call from server.
+    static async #get_sock_mgr(ctx) {
+        let server = Server.current();
+        if (!server || !ctx || !(await server.request_loggedin(ctx.req)))
+            return;
+        return server.sock_mgr;
+    }
+
+    static async call(src_id, name, params, ctx) {
+        let sock_mgr = await this.#get_sock_mgr(ctx);
+        if (!sock_mgr)
+            return;
+        return sock_mgr.call_method(src_id, name, params);
+    }
+    static async get(raw_params, return_age, ctx) {
+        let sock_mgr = await this.#get_sock_mgr(ctx);
+        if (!sock_mgr)
+            return return_age ? [undefined, 0] : undefined;
+        // The age is managed on the client side by the socket to save communication bandwidth.
+        // We don't need to do this on the server side (there's no point caching the values
+        // that are already in the `SocketManager`)
+        // and we can simply get the values from the `SocketManager` directly.
+        // Simply translate the parameters to the right format and forward it.
+        let params = Object.create(null);
+        for (let src_id of Object.getOwnPropertyNames(raw_params))
+            params[src_id] = { path: raw_params[src_id] };
+        let raw_res = await sock_mgr.get_values(params);
+        let res = Object.create(null);
+        for (let src_id of Object.getOwnPropertyNames(raw_res))
+            res[src_id] = raw_res[src_id].values;
+        if (!return_age)
+            return res;
+        let ages = Object.create(null);
+        for (let src_id of Object.getOwnPropertyNames(raw_res)) {
+            let age = raw_res[src_id].age;
+            let copy_age = (param) => {
+                if (!is_object(param))
+                    return age;
+                let ages = Object.create(null);
+                for (let name of Object.getOwnPropertyNames(param))
+                    ages[name] = copy_age(param[name]);
+                return ages;
+            };
+            ages[src_id] = copy_age(raw_params[src_id]);
+        }
+        return [res, ages];
+    }
+    static async set(params, ctx) {
+        let sock_mgr = await this.#get_sock_mgr(ctx);
+        if (!sock_mgr)
+            return;
+        return sock_mgr.set_values(params);
+    }
 };
 
 module.exports = socket;
