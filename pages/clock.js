009e5b2143d2026bbe76a8794376c3b9f48dd033
Yichao Yu <yyc1992@gmail.com>
2021-02-08 10:54:53 -0500

    Add zynq test pages

---
+++ pages/clock.js
@@ -0,0 +1,34 @@
+/*************************************************************************
+ *   Copyright (c) 2019 - 2019 Yichao Yu <yyc1992@gmail.com>             *
+ *                                                                       *
+ *   This library is free software; you can redistribute it and/or       *
+ *   modify it under the terms of the GNU Lesser General Public          *
+ *   License as published by the Free Software Foundation; either        *
+ *   version 3.0 of the License, or (at your option) any later version.  *
+ *                                                                       *
+ *   This library is distributed in the hope that it will be useful,     *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of      *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU    *
+ *   Lesser General Public License for more details.                     *
+ *                                                                       *
+ *   You should have received a copy of the GNU Lesser General Public    *
+ *   License along with this library. If not,                            *
+ *   see <http://www.gnu.org/licenses/>.                                 *
+ *************************************************************************/
+
+"use strict";
+
+import Wrapper from '../components/Wrapper';
+import CheckLogin from '../components/CheckLogin';
+
+import { Pages } from '../components/SourceWidgets';
+
+export default function Default() {
+    return (
+        <Wrapper>
+          <CheckLogin>
+            <Pages.zynq.clock.widget source_id={"zynq-1"}/>
+          </CheckLogin>
+        </Wrapper>
+    );
+}
